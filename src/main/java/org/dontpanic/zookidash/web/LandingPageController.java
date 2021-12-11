6320ae4f8f0df34da9cfabcb569e383bd82c59fa
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2021-12-11 11:23:12 +0000

    Basic landing page

---
+++ src/main/java/org/dontpanic/zookidash/web/LandingPageController.java
@@ -0,0 +1,14 @@
+package org.dontpanic.zookidash.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LandingPageController {
+
+    @RequestMapping("/")
+    public String landingPage() {
+        return "index";
+    }
+
+}
