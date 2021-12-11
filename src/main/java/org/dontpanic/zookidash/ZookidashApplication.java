e1d865700e9a9b79f841ea12d9fa1f0b836eeda8
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2021-12-11 11:02:32 +0000

    Initialize from start.spring.io

---
+++ src/main/java/org/dontpanic/zookidash/ZookidashApplication.java
@@ -0,0 +1,13 @@
+package org.dontpanic.zookidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookidashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookidashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dontpanic/zookidash/ZookidashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dontpanic.zookidash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookidashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
